761719debf164ec3df799152c77702a29d7edda2
Kenneth <crazedpokerpro@yahoo.com>
2015-09-08 17:04:23 -0400

    redo migrations

--- db/migrate/20150817200904_create_products.rb
+++ db/migrate/20150817200904_create_products.rb
@@ -4,7 +4,7 @@ class CreateProducts < ActiveRecord::Migration
       t.string :name
       t.text :description
       t.string :image_url
-      t.string :colour
+      
       
       
       t.timestamps null: false
