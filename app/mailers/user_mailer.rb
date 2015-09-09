db230128cedd200ed17520ce486c25131a7a6232
Kenneth <crazedpokerpro@yahoo.com>
2015-09-09 13:48:39 -0400

    usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,14 +4,9 @@ default from: "from@example.com"
   def contact_form(email, name, message)
     @message = message
     mail(:from => email, 
-            :to => 'your-email@example.com', 
+            :to => 'crazedpokerplayer@mail.com', 
             :subject => "A new contact form message from #{name}")
   end
 
-  def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  UserMailer.contact_form(@email, @name, @message).deliver
-  end
+
 end
