053225f0cdf5dce7104436657657d0ca5ddb228e
Kenneth <crazedpokerpro@yahoo.com>
2015-09-09 13:40:31 -0400

    More email fixes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,11 @@ class StaticPagesController < ApplicationController
   def index
      
   end
+  
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver
+  end
 end
