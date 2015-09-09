168c7b6b3679cf23d00cde5081c636ded61bc139
Kenneth <crazedpokerpro@yahoo.com>
2015-09-09 17:40:28 -0400

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+   protected
+
+#after successful signin, redirect to products page like a boss.
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+  products_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
+
 end
