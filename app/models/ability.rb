8d9de7eb7a6c9005a49b6395da4563051c69e9ca
Kenneth <crazedpokerpro@yahoo.com>
2015-09-16 15:17:33 -0400

    ex 45 admin role + images in product#show

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+      else
+        can :manage, User, id: user.id
+        can :read, :all
+    end
   end
 
   
